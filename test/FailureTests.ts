9bd8cb58d9e810257b3fb2835642116b62c0f083
Bryan Phelps <bryphe@outlook.com>
2018-05-07 09:54:04 -0700

    Start prototyping testing the CLI output

---
+++ test/FailureTests.ts
@@ -0,0 +1,26 @@
+import * as assert from "assert"
+
+import * as path from "path"
+
+import { execSync, spawnSync } from "child_process"
+
+const binPath = process.execPath
+const coreScriptPath = path.join(__dirname, "..", "..", "bin", "cli.js")
+
+describe("failure cases", () => {
+    it("logs failure", () => {
+        console.log(binPath + " " + coreScriptPath)
+
+        let result = null
+        try {
+            result = spawnSync(binPath, [coreScriptPath], {}).stdout.toString("utf8")
+        } catch (err) {
+            console.log("return code: " + err.status)
+        }
+
+        console.log("output: " + result)
+
+        console.log("done")
+        assert.strictEqual(0, 1)
+    })
+})
