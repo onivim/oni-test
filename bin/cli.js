71973e6d83c6cb8cc6d4432bfdf1ab77accc63ac
Bryan Phelps <bryphe@outlook.com>
2018-05-14 11:10:27 -0700

    Add --develop flag to run against a local development build, and add ability to specify a test path

--- bin/cli.js
+++ bin/cli.js
@@ -5,12 +5,26 @@
 const parseArgs = require("minimist")
 
 const result = parseArgs(process.argv)
+const os = require("os")
 
 const args = result["_"]
 const testPath = args[args.length - 1]
 
 const path = require("path")
 const OniTest = require("./../lib/src/OniTest")
+
+let executablePath, executableArgs
+
+if (result["develop"]) {
+    const electronCommand = os.platform() === "win32" ? "electron.cmd" : "electron"
+    executablePath = path.join(result["develop"], "node_modules", ".bin", electronCommand)
+
+    const mainJsPath = path.join(path.join(result["develop"]), "lib", "main", "src", "main.js")
+    executableArgs = [mainJsPath]
+} else {
+    executablePath = process.env["ONI_EXECUTABLE_PATH"]
+    executableArgs = []
+}
 
 const test = new OniTest.OniTest(
     {
