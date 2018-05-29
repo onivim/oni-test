9cabdc3be49569de423528f8860069141cb7f2eb
Bryan Phelps <bryphe@outlook.com>
2018-05-29 13:28:14 -0700

    Fix substitution again

--- build/build-oni.sh
+++ build/build-oni.sh
@@ -4,6 +4,6 @@ echo Travis build - detected OS is: "$TRAVIS_OS_NAME"
 set -e
 
 ovm install master
-export ONI_EXECUTABLE_PATH="${ovm path master}"
+export ONI_EXECUTABLE_PATH="$(ovm path master)"
 
 echo Oni executable path: $ONI_EXECUTABLE_PATH
