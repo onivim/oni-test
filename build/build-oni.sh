e559fb2d19f542e1f069471c3cee69d4d185f199
Bryan Phelps <bryphe@outlook.com>
2018-05-29 11:49:42 -0700

    Use the new 'ovm' utility for getting builds

--- build/build-oni.sh
+++ build/build-oni.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-# TODO: This is a stub for now to get an Oni build to run on CI,
-# but the plan is eventually to expose a CLI utility for getting
-# a specific Oni version.
-
 echo Travis build - detected OS is: "$TRAVIS_OS_NAME"
 set -e
 
-git clone https://github.com/onivim/oni.git .local-oni
-cd .local-oni
+ovm install master
+export ONI_EXECUTABLE_PATH = ovm path master
 
-echo Building Oni in: "$PWD"
-
-yarn install
-yarn check-cached-binaries
-yarn build
-yarn run pack --dir
-
-ls -a dist
+echo Oni executable path: $ONI_EXECUTABLE_PATH
