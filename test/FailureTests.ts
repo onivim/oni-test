4ed2d139aebd553f5a910af98b5d174525749e16
Bryan Phelps <bryphe@outlook.com>
2018-05-29 13:50:26 -0700

    Add both success / failure tests

--- test/FailureTests.ts
+++ test/FailureTests.ts
@@ -2,22 +2,25 @@ import * as assert from "assert"
 
 import * as path from "path"
 
-import { execSync, spawnSync } from "child_process"
+import * as cp from "child_process"
 
 const binPath = process.execPath
 const coreScriptPath = path.join(__dirname, "..", "..", "bin", "cli.js")
+const failureTestPath = path.join(__dirname, "..", "..", "test_collateral", "ShouldFailTest.js")
 
 describe("failure cases", () => {
     it("logs failure", () => {
         console.log(binPath + " " + coreScriptPath)
 
-        let result = null
+        let result: cp.SpawnSyncReturns<Buffer> = null
         try {
-            result = spawnSync(binPath, [coreScriptPath], {}).stdout.toString("utf8")
-        } catch (err) {
-            console.log("return code: " + err.status)
-        }
+            result = cp.spawnSync(binPath, [coreScriptPath, failureTestPath], {})
+        } catch (err) {}
 
-        console.log("output: " + result)
+        assert.strictEqual(
+            result.status,
+            1,
+            "Status should be 1, since this is running a failure test",
+        )
     })
 })
---
+++ test/SuccessTests.ts
@@ -0,0 +1,20 @@
+import * as assert from "assert"
+
+import * as path from "path"
+
+import * as cp from "child_process"
+
+const binPath = process.execPath
+const coreScriptPath = path.join(__dirname, "..", "..", "bin", "cli.js")
+const successTestPah = path.join(__dirname, "..", "..", "test_collateral", "ShouldPassTest.js")
+
+describe("success cases", () => {
+    it("logs success", () => {
+        let result: cp.SpawnSyncReturns<Buffer> = null
+        try {
+            result = cp.spawnSync(binPath, [coreScriptPath, successTestPah], {})
+        } catch (err) {}
+
+        assert.strictEqual(result.status, 0, "Status should be 0, since the test passed")
+    })
+})
